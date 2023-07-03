f6ec651890d7f840fa73c9a9346537e93ab6733d
GregoM <actar9@gmail.com>
2023-07-03 12:51:38 -0300

    first commit develop

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,33 @@
 package main
 
-import()
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	var (
+		ConnectionString = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
+			os.Getenv("user"),
+			os.Getenv("pass"),
+			os.Getenv("hostdb"),
+			os.Getenv("port"),
+			os.Getenv("db_name"))
+	)
+	fmt.Print(ConnectionString)
+
+	db, err := sql.Open("mysql", ConnectionString)
+	if err != nil {
+		log.Fatal("Error opening database")
+	}
+	
+	fmt.Print(db)
 
 }
